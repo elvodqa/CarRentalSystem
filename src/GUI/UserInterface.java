1befe8f4cbff92d68cd8112dd30ab47dd538794c
armin <arminhappy2@gmail.com>
2025-12-07 12:25:13 -0500

    Update

--- src/GUI/UserInterface.java
+++ src/GUI/UserInterface.java
@@ -1,20 +1,26 @@
 package GUI;
 import Database.Database;
+import Models.Car;
 import Models.User;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UserInterface extends JFrame
 {
     private static Database db;
-    private static JPanel cardPanel, card1, card2, card3;
-    private static JLabel label1, label2, label3, label4, label5, label6;
-    private static JTextField text1, text2, text3, text4, text5, text6;
-    private static JButton button1, button2, button3, button4, button5;
+    private static JPanel cardPanel, card1, card2, card3, card4;
+    private static JLabel label1, label2, label3, label4, label5, label6, label7, label8, label9;
+    private static JTextField text1, text2, text3, text4, text5, text6, text7;
+    private static JButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    private static JList <Car> listBox;
+    private static JComboBox<Car> comboBox;
+    private static DefaultListModel<Car> vehicles = new DefaultListModel<>();
     private static java.awt.CardLayout cardLayout = new java.awt.CardLayout();
 
 
@@ -23,7 +29,7 @@ public class UserInterface extends JFrame
         this.db = db;
 
         setTitle("User Interface");
-        setSize(600,400);
+        setSize(900,600);
 
         cardPanel = new JPanel();
         cardPanel.setLayout(cardLayout);
@@ -38,12 +44,19 @@ public class UserInterface extends JFrame
         label4 = new JLabel("Last Name");
         label5 = new JLabel("Email Name");
         label6 = new JLabel("Password");
+        label7 = new JLabel("List of Vehicles");
+        label8 = new JLabel("Invoice");
+        label9 = new JLabel("Vehicle Selection");
 
         button1 = new JButton("Go to Create Account");
         button2 = new JButton("Login");
         button3 = new JButton("Create Account");
         button4 = new JButton("Back to Home");
-        button5 = new JButton("Home");
+        button5 = new JButton("Rent Vehicle");
+        button6 = new JButton("Search Vehicles");
+        button7 = new JButton("Cancel Rental");
+        button8 = new JButton("Generate Invoice");
+        button9 = new JButton("Back to Login");
 
         text1 = new JTextField("",15);
         text2 = new JTextField("",15);
@@ -51,6 +64,20 @@ public class UserInterface extends JFrame
         text4 = new JTextField("",15);
         text5 = new JTextField("",15);
         text6 = new JTextField("",15);
+        text7 = new JTextField("",15);
+
+        listBox = new JList<>(vehicles);
+        listBox.setVisibleRowCount(-1);
+        listBox.setVisible(true);
+        listBox.setSize(100, 100);
+        JScrollPane scrollPane = new JScrollPane(listBox);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setSize(100,100);
+
+        comboBox = new JComboBox<>();
+        comboBox.setSelectedIndex(-1);
+        comboBox.setVisible(true);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -58,14 +85,20 @@ public class UserInterface extends JFrame
         Insets in = new Insets(10,10,10,10);
         gbc.insets = in;
 
+        Dimension fieldSize = new Dimension(200, 30);
+
         gbc.gridx = 0; gbc.gridy = 0;
         card1.add(label1,gbc);
+        label1.setPreferredSize(fieldSize);
         gbc.gridx = 1; gbc.gridy = 0;
         card1.add(text1,gbc);
+        text1.setPreferredSize(fieldSize);
         gbc.gridx = 0; gbc.gridy = 1;
         card1.add(label2,gbc);
+        label2.setPreferredSize(fieldSize);
         gbc.gridx = 1; gbc.gridy = 1;
         card1.add(text2,gbc);
+        text2.setPreferredSize(fieldSize);
 
 
         gbc.gridx = 0; gbc.gridy = 0;
@@ -85,8 +118,6 @@ public class UserInterface extends JFrame
         gbc.gridx = 1; gbc.gridy = 3;
         card2.add(text6, gbc);
 
-
-        Dimension fieldSize = new Dimension(200, 30);
 
         button1.addActionListener(new ActionListener() {
             @Override
@@ -121,12 +152,15 @@ public class UserInterface extends JFrame
                     if (text1.getText().isEmpty() || text2.getText().isEmpty())
                     {
                         JOptionPane.showMessageDialog(null, "Error found in adding account.");
+                        return;
                     }
+
                     validation = db.validateUser(text1.getText(), text2.getText());
 
                     if (validation == true)
                     {
                         cardLayout.show(cardPanel, "3");
+                        // System.out.print(text1.getText());
                         text1.setText("");
                         text2.setText("");
                         text3.setText("");
@@ -208,19 +242,58 @@ public class UserInterface extends JFrame
             {
                 String command = e.getActionCommand();
 
-                if (command == "Home")
+                if (command == "Rent Vehicle")
                 {
-                    cardLayout.show(cardPanel, "1");
-                    text1.setText("");
-                    text2.setText("");
-                    text3.setText("");
-                    text4.setText("");
-                    text5.setText("");
-                    text6.setText("");
+                    // comboBox.getSelectedItem()
+
                 }
             }
         });
         button5.setPreferredSize(fieldSize);
+
+        button6.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                String command = e.getActionCommand();
+
+                if (command == "Search Vehicles")
+                {
+                    vehicles.clear();
+                    comboBox.removeAllItems();
+                    List<Car> cars = new ArrayList<>();
+                    cars = db.getAllCars();
+
+                    for (Car car : cars)
+                    {
+                        vehicles.addElement(car);
+                        comboBox.addItem(car);
+                    }
+
+                    listBox.setModel(vehicles);
+                }
+            }
+        });
+        button6.setPreferredSize(fieldSize);
+
+        button9.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                String command = e.getActionCommand();
+
+                if (command == "Back to Login")
+                {
+
+                }
+            }
+        });
+
+
+        fieldSize = new Dimension(200, 30);
+
 
         gbc.gridx = 0; gbc.gridy = 2;
         card1.add(button1,gbc);
@@ -237,7 +310,40 @@ public class UserInterface extends JFrame
         button2.setPreferredSize(fieldSize);
 
         gbc.gridx = 0; gbc.gridy = 0;
+        card3.add(label7, gbc);
+        gbc.gridx = 2; gbc.gridy = 0;
+        card3.add(label8, gbc);
+        fieldSize = new Dimension (100, 100);
+        gbc.gridwidth = 2;
+        gbc.gridx = 0; gbc.gridy = 1;
+        card3.add(scrollPane, gbc);
+        scrollPane.setPreferredSize(fieldSize);
+        gbc.gridx = 2; gbc.gridy = 1;
+        card3.add(text7, gbc);
+        text7.setPreferredSize(fieldSize);
+        gbc.gridwidth = 1;
+        fieldSize = new Dimension (200, 30);
+        gbc.gridx = 0; gbc.gridy = 2;
+        card3.add(comboBox, gbc);
+        gbc.gridx = 0; gbc.gridy = 3;
         card3.add(button5, gbc);
+        button5.setPreferredSize(fieldSize);
+        gbc.gridx = 1; gbc.gridy = 3;
+        card3.add(button6,gbc);
+        button6.setPreferredSize(fieldSize);
+        gbc.gridx = 2; gbc.gridy = 3;
+        card3.add(button7, gbc);
+        button7.setPreferredSize(fieldSize);
+        gbc.gridx = 3; gbc.gridy = 3;
+        card3.add(button8, gbc);
+        button8.setPreferredSize(fieldSize);
+        gbc.gridwidth = 2;
+        gbc.gridx = 1; gbc.gridy = 4;
+        card3.add(button9, gbc);
+        button9.setPreferredSize(fieldSize);
+        gbc.gridwidth = 1;
+
+        // Ask for a user's info function in backend
 
         cardPanel.add(card1, "1");
         cardPanel.add(card2, "2");
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 import Database.Database;
 import Models.User;
+import Models.Car;
 
 public class Test {
     public static void main(String[] args) {
@@ -7,12 +8,19 @@ public class Test {
         db.createTables();
         db.createUser("John", "Doe", "johndoe@gmail.com", "password123");
         db.createUser("Jane", "Smith", "janesmith@gmail.com", "securepass");
+        db.createUser("A", "R", "arminhappy2@gmail.com", "123");
+        db.createCar("Forester", "2018", "FSI1123", "Grey", 500);
+        db.createCar("Explorer", "2024", "SAC0312", "Red", 1000);
 
         System.out.println("Users created successfully.");
         for (User userInfo : db.getAllUsers()) {
             System.out.println(userInfo);
         }
 
+        for (Car carInfo : db.getAllCars())
+        {
+            System.out.println(carInfo);
+        }
 
 
     }
