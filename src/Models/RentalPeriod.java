faf956fdf8a7ddde3326f7371e62e8bf908df536
armin <arminhappy2@gmail.com>
2025-12-08 11:15:36 -0500

    Update

--- src/Models/RentalPeriod.java
+++ src/Models/RentalPeriod.java
@@ -9,12 +9,31 @@ public class RentalPeriod {
     public Date startDate;
     public Date endDate;
 
-    public RentalPeriod(int id, int userId, int carId, Date startDate, Date endDate) {
+    public RentalPeriod(int id, int userId, int carId, Date startDate, Date endDate)
+    {
         this.id = id;
         this.userId = userId;
         this.carId = carId;
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    public void setRentalPeriod(int id, int userId, int carId, Date startDate, Date endDate)
+    {
+        this.id = id;
+        this.userId = userId;
+        this.carId = carId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void getRentalPeriod()
+    {
+        System.out.println("Rental Period ID: " + this.id);
+        System.out.println("User ID: " + this.userId);
+        System.out.println("Car ID: " + this.carId);
+        System.out.println("Start Date: " + this.startDate);
+        System.out.println("End Date: " + this.endDate);
     }
 
     public String toString() {
